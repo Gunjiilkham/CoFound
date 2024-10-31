333042131a614bbc0284a0406ddc79b3fdcc90d4
naomi19s26 <aisosaenogieru94@gmail.com>
2024-10-31 10:15:49 -0400

    Modified login page layout!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,35 +8,31 @@ const Page: React.FC = () => {
                 <meta charSet="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>Tindernships</title>
-                {/* Link to your CSS file */}
-                {/* <link rel="stylesheet" href="styles.css" /> */}
             </Head>
-            <nav>
-                <ul>
-                    <li><a href="#home">Home</a></li> 
-                    <li><a href="#user">Profile</a></li>
-                    <li><a href="#listings">Listings</a></li>
-                    <li><a href="#logIn">Log In</a></li>
-                </ul>
-            </nav>
-            <header>
-                <h1>Tindernships</h1>
-                <h2>Love Your Career</h2>
+
+            <div style={{ backgroundColor: 'black', color: 'white', height: '100vh', margin: 0 }}>
+            <header style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center', height: '50vh' }}>
+                <h1 style={{ fontWeight: 'bold', fontSize: '3em' }}>Tindernship</h1>
+                <h2 style={{ fontWeight: 'bold', fontSize: '2em' }}>Love Your Career</h2>
             </header>
+
             <main>
                 <section id="home">
+                    <center>
                     <p>Discover personalized internship suggestions in only a few swipes.</p>
-                    <ul>
-                        <li><a href="#join">Sign Up</a></li>
-                    </ul>
+                    <button>Click Me!</button>
+                    </center>
                 </section>
             </main>
-            <footer>
-                <p>&copy; 2024 Tindernships. All rights reserved.</p>
+
+
+            <footer style={{ textAlign: 'center', padding: '20px',}}>
+                    <p>&copy; 2024 Tindernships. All rights reserved.</p>
             </footer>
+
+            </div>
         </>
     );
 };
 
-export default Page;  
-
+export default Page;
